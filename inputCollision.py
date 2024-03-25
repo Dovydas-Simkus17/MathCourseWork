cd38285d8f4fa4f713b3f20b9d3304483e8c7857
Dovydas-Simkus17 <145037890+Dovydas-Simkus17@users.noreply.github.com>
2024-03-25 15:21:08 +0000

    Update inputCollision.py

--- inputCollision.py
+++ inputCollision.py
@@ -215,7 +215,11 @@ while(status):
                     forc = int(user_text)
                     print(forc)
                     user_text = ''
-                    ball1.index = 0
+                    for ball in balls:
+                        ball.index = 0
+                        ball.hitForce = 0
+                        ball.totalMilliseconds = 0
+                        ball.totalVelocity = 0
                     ball1.hitForce = forc
 
                 elif event.key == p.K_BACKSPACE:
