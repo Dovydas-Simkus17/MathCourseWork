02c661c9b746ebef4044d194900c7aefdcc739f1
Ostin <91962782+Nitso-93@users.noreply.github.com>
2024-04-08 15:12:53 +0100

    Add files via upload

--- CueStickCode.py
+++ CueStickCode.py
@@ -107,7 +107,7 @@ ball13 = create_ball(ball_diameter/2,(230,300))
 ball14 = create_ball(ball_diameter/2,(230,336))
 ball15 = create_ball(ball_diameter/2,(230,372))
 
-balls = {ball1,ball2,ball3,ball4,ball5,ball6,ball7,ball8,ball9,ball10,ball11,ball12,ball13,ball14,ball15}
+balls = {ball1,ball2,ball3,ball4,ball5,ball6,ball7,ball8,ball9,ball10,ball11,ball12,ball13,ball14,ball15,cue_ball}
 
 
 #Create Pool Table Cushions
